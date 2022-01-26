bff53c7e722c0050cade734845f7f214353a3f42
Abby Lee <aal86@georgetown.edu>
2022-01-26 18:20:29 -0500

    Asked for user input

--- game.py
+++ game.py
@@ -5,3 +5,35 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+# ASK FOR USER INPUT
+PlayerOne = input("Please choose one of the following : 'rock', 'paper', or 'scissors': ")
+
+print(PlayerOne)
+
+PlayerTwo = input("Please choose one of the following : 'rock', 'paper', or 'scissors': ")
+
+print(PlayerOne)
+
+# VALIDATIONS
+
+
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+
+
+
+# FINAL RESULTS
+
+
+
+
+
+
