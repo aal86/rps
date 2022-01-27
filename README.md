d2ac3d597ad59ef87b8a8b8a9a80b3de060f3411
Abby Lee <aal86@georgetown.edu>
2022-01-27 14:34:48 -0500

    Added info to READ ME file

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Create a virtual environment:
 ```sh
 conda create -n rps-env python=3.8
 ```
+    first time only
 
 Activate the virtual environment:
 
@@ -21,6 +22,63 @@ Install package dependencies (mainly for testing):
 ```sh
 pip install -r requirements.txt
 ```
+
+## Requirements
+
+Asks for the user input using the input function while giving the user the options between rock, paper, or scissors
+
+Uses the .lower() function to make the user's input lowercase to avoid case sensitive testing 
+
+Uses the print function to print the user's input
+
+Uses an if statement to validate that the user's input is either rock, scissors, or paper and if it is false exits the program
+
+Imports the random module
+
+Creates a list of valid choices and passes them to the random choice function
+
+Uses the print function to print the computer's input
+
+Calls the global function determine_winner(PlayerOne, ComputerChoice) to determine the winter and print the outputs of the game
+
+Uses the print function to thank the user for playing and welcomes them to play again
+
+## Further Exploration
+Install the package 
+
+    ```sh
+    pip install python-dotenv
+    ```
+Create an .env file and defines the Player_Name variable where the user must input their own name under this file so the game.py file can read it
+    For example: Player_Name = "Abby" defines the environment variable to the name Abby
+
+Within the requirements.txt must add python-dotenv to allow the environment variables to work
+
+Import the os module to enable environment variables in the game.py file
+
+Add the lines below into the game.py file to access and use the environment variables
+    from dotenv import load_dotenv
+    load_dotenv()
+
+Define the player's name in the game.py file using os.getenv passing the environment variable creating in the .env file followed by a comma and setting the default player's name to Player One
+
+Uses the print function to welcome the player to the game using the environment variable to print the player's name as well
+
+Defines the function determine_winner globally with two parameters, player one's input and the computer's choice
+
+determine_winner function has mutliple conditional statements to test all nine possibilities and prints and returns the results
+
+Creates the main to seperate the global function from the main 
+
+Add another file called game_test.py which tests the nine possible outcome and contains the line below to access the function in the game.py file
+    from game import determine_winner
+
+Install the pytest package
+    ```sh
+    pip install pytest
+    ```
+
+Add 'pytest' to the requirements.txt to ensure pytest package has been installed into the active virtual environment
 
 ## Usage
 
--- game.py
+++ game.py
@@ -1,72 +1,97 @@
-
-
-
 #
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+#AUTOMATED TESTING (GLOBAL)
+#defines global function with two parameters, player one's input and the computer's choice
+def determine_winner (PlayerOne, ComputerChoice):
+    if PlayerOne == ComputerChoice:
+        print("Both players selected " + PlayerOne + ". It's a tie!")
+        return(None)
+    elif PlayerOne == "rock":
+        if ComputerChoice == "paper":
+            print("Paper beats rock! You lose.")
+            return("paper")
+        else:
+            print("Rock beats scissors! You won!")
+            return("rock")
+    elif PlayerOne == "paper":
+        if ComputerChoice == "rock":
+            print("Paper beats rock! You won!")
+            return("paper")
+        else:
+            print("Scissors beats paper! You lose.")
+            return("scissors")
+    elif PlayerOne == "scissors":
+        if ComputerChoice == "rock":
+            print("Rock beats scissors! You lose.")
+            return("rock")
+        else:
+            print("Scissors beats paper! You won!")
+            return("scissors")
+
+#MAIN
+if __name__ == "__main__":
+
 #WELCOME MESSAGE
-import os
+#import os to enable environment variables
+    import os
 
-from dotenv import load_dotenv
-load_dotenv()
+    from dotenv import load_dotenv
+    load_dotenv()
 
-PlayerName = os.getenv('Player_Name', default = "Player One")
+#gets the environment variable 'Player_Name' and if there isn't one, defaults PlayerName to Player One
+    PlayerName = os.getenv('Player_Name', default = "Player One")
 
-print("Welcome " + PlayerName + " to my Rock-Paper-Scissors game...")
+    print("Welcome " + PlayerName + " to my Rock-Paper-Scissors game...")
 
 # ASK FOR USER INPUT
-PlayerOne = input("Please choose either : 'rock', 'paper', or 'scissors': ")
-PlayerOne = PlayerOne.lower()
+    PlayerOne = input("Please choose either : 'rock', 'paper', or 'scissors': ")
 
-print("You chose: " + PlayerOne)
+#makes the user input all lowercase 
+    PlayerOne = PlayerOne.lower()
+
+    print("You chose: " + PlayerOne)
 
 # VALIDATIONS
-
-if PlayerOne != "rock" and PlayerOne != "scissors" and PlayerOne != "paper":
-    exit()
+    if PlayerOne != "rock" and PlayerOne != "scissors" and PlayerOne != "paper":
+        exit()
 
 # COMPUTER CHOICE
-import random
+    import random
 
-PossibleChoices = ("rock", "paper", "scissors")
-ComputerChoice = random.choice(PossibleChoices)
-print("The computer chose: " + ComputerChoice)
-
-
+    PossibleChoices = ("rock", "paper", "scissors")
+    ComputerChoice = random.choice(PossibleChoices)
+    print("The computer chose: " + ComputerChoice)
 
 # DETERMINE THE WINNER
+#calls the global function within the main to determine the winner
+    determine_winner(PlayerOne, ComputerChoice)
 
-if PlayerOne == ComputerChoice:
-    print("Both players selected " + PlayerOne + ". It's a tie!")
-elif PlayerOne == "rock":
-    if ComputerChoice == "scissors":
-        print("Rock beats scissors! You win!")
-    else:
-        print("Paper beats rock! You lose.")
-elif PlayerOne == "paper":
-    if ComputerChoice == "rock":
-        print("Paper beats rock! You win!")
-    else:
-        print("Scissors beats paper! You lose.")
-elif PlayerOne == "scissors":
-    if ComputerChoice == "paper":
-        print("Scissors beats paper! You win!")
-    else:
-        print("Rock beats scissors! You lose.")
+    # if PlayerOne == ComputerChoice:
+    #     print("Both players selected " + PlayerOne + ". It's a tie!")
+    # elif PlayerOne == "rock":
+    #     if ComputerChoice == "scissors":
+    #         print("Rock beats scissors! You win!")
+    #     else:
+    #         print("Paper beats rock! You lose.")
+    # elif PlayerOne == "paper":
+    #     if ComputerChoice == "rock":
+    #         print("Paper beats rock! You win!")
+    #     else:
+    #         print("Scissors beats paper! You lose.")
+    # elif PlayerOne == "scissors":
+    #     if ComputerChoice == "paper":
+    #         print("Scissors beats paper! You win!")
+    #     else:
+    #         print("Rock beats scissors! You lose.")
 
 
 # FINAL RESULTS
-print("Thanks for playing. Please play again!")
+    print("Thanks for playing. Please play again!")
 
-#AUTOMATED TESTING
-def determine_winner (choice_1, choice_2):
-    PlayerOne = choice_1
-    ComputerChoice = choice_2
 
-determine_winner(PlayerOne, ComputerChoice)
-
-    
+        
     
 
 
